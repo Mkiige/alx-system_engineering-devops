038ecb82d01db4ec5850d37067e14b2577505533
Mkiige <mkiige77@gmail.com>
2023-01-11 16:23:21 +0300

    config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# connect to a server Execute a command
+Host 3.235.75.142
+    User Mkiige
+    IdentityFile ~/.ssh/school
+    PreferredAuthentications publickey
+    PasswordAuthentication no
