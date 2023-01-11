72001f14118a012f717f7455e0de8f3324eedaa5
Mkiige <mkiige77@gmail.com>
2023-01-11 16:28:33 +0300

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # connect to a server Execute a command
-Host 3.235.75.143
-    User Mkiige
-    IdentityFile ~/.ssh/school
-    PreferredAuthentications publickey
-    PasswordAuthentication no
+
+exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
+        path    => '/bin/'
+}
